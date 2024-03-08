b2cc625a56991d0b3d6aa71fd455c316fa50a03d
Divash <dpokharel@stu.jsu.edu>
2024-03-08 12:37:19 -0600

    Passed the PunchCreate test!!!

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -62,7 +62,7 @@ public class Punch {
         this.badge = badge;
     }
 
-    public LocalDateTime getOriginalTimestamp() {
+    public LocalDateTime getOriginalTimestamp(){
         return originalTimestamp;
     }
 
@@ -98,7 +98,6 @@ public class Punch {
         StringBuilder s = new StringBuilder();
         
         DateTimeFormatter newformat = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm:ss");
-        
         s.append("#");
         s.append(badge.getId()).append(" ");
         s.append(punchType).append(": ");
